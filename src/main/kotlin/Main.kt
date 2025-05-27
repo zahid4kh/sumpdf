f45ff4b919b6a8778fcfaeddee237fb717aee7c5
zahid4kh <halilzahid@gmail.com>
2025-05-27 10:54:37 +0200

    upd window details + added app icon parameter (needed for windows)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,16 @@
-@file:JvmName("SumPDF") // Must have no spaces!!!
+@file:JvmName("SumPDF")
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import org.jetbrains.compose.resources.painterResource
 import theme.AppTheme
 import java.awt.Dimension
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.getKoin
+import sumpdf.resources.Res
+import sumpdf.resources.sumpdf
 
 fun main() = application {
     startKoin {
@@ -19,7 +22,9 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         state = rememberWindowState(size = DpSize(800.dp, 600.dp)),
-        title = "SumPDF - Made with Compose for Desktop"
+        title = "SumPDF",
+        alwaysOnTop = true,
+        icon = painterResource(Res.drawable.sumpdf)
     ) {
         window.minimumSize = Dimension(800, 600)
 
