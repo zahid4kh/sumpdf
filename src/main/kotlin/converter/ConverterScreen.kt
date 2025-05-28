d1bc388f3f944e0e2dccc33e0cda5944b618b4ff
zahid4kh <halilzahid@gmail.com>
2025-05-28 18:01:21 +0200

    .

--- src/main/kotlin/converter/ConverterScreen.kt
+++ src/main/kotlin/converter/ConverterScreen.kt
@@ -5,8 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
