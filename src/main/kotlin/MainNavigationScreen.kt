dc32408708d3cfb785323f002a8ef8f5cd574f1e
zahid4kh <halilzahid@gmail.com>
2025-06-07 19:15:39 +0200

    added "about" dialog

--- src/main/kotlin/MainNavigationScreen.kt
+++ src/main/kotlin/MainNavigationScreen.kt
@@ -1,8 +1,11 @@
-import androidx.compose.animation.core.animateSizeAsState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material.icons.filled.Transform
 import androidx.compose.material3.*
@@ -10,19 +13,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import deskit.dialogs.InfoDialog
+import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle
+import org.apache.batik.svggen.SVGCSSStyler.style
 import org.jetbrains.compose.resources.painterResource
 import sumpdf.resources.Res
 import sumpdf.resources.combine_svgrepo_com
+import sumpdf.resources.sumpdf
+import sumpdf.resources.vectorsumpdf
+import java.awt.Desktop
+import java.net.URI
+import java.net.URL
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,6 +46,61 @@ fun MainNavigationScreen(
     onNavigateToConverter: () -> Unit
 ) {
     val cardHeight by remember { mutableStateOf(370.dp) }
+    var showAppInfo by remember { mutableStateOf(false)}
+
+    if (showAppInfo) {
+        InfoDialog(
+            width = 450.dp,
+            height = 370.dp,
+            title = "About SumPDF",
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+            onClose = { showAppInfo = false },
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        .verticalScroll(rememberScrollState()),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    Text(
+                        text = "SumPDF",
+                        style = MaterialTheme.typography.headlineSmall,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                    Text(
+                        text = "Version: 1.1.0",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Text(
+                        text = "Developer: zahid4kh",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier
+                            .clickable{
+                                Desktop.getDesktop().browse(URI("https://github.com/zahid4kh"))
+                            }
+                            .pointerHoverIcon(icon = PointerIcon.Hand)
+                    )
+                    Text(
+                        text = "License: Apache 2.0",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "SumPDF is a free and open-source desktop application that allows you to effortlessly combine multiple PDF files and convert various file types to PDF format.",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        textAlign = TextAlign.Start
+                    )
+                }
+            }
+        )
+    }
 
     Scaffold(
         topBar = {
@@ -52,6 +120,17 @@ fun MainNavigationScreen(
                         Icon(
                             imageVector = if (darkMode) Icons.Default.LightMode else Icons.Default.DarkMode,
                             contentDescription = if (darkMode) "Switch to Light Mode" else "Switch to Dark Mode"
+                        )
+                    }
+
+                    IconButton(
+                        onClick = {showAppInfo = true},
+                        modifier = Modifier
+                            .pointerHoverIcon(icon = PointerIcon.Hand),
+                    ){
+                        Icon(
+                            imageVector = Icons.Default.Info,
+                            contentDescription = "App Info"
                         )
                     }
                 },
