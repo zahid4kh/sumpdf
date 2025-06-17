a2aa3068f3c71db965928cbfd0bf92f54ce25b2e
zahid4kh <halilzahid@gmail.com>
2025-06-17 11:51:23 +0200

    added SplitterViewModel to app module

--- src/main/kotlin/AppModule.kt
+++ src/main/kotlin/AppModule.kt
@@ -2,6 +2,7 @@ import combiner.CombinerViewModel
 import converter.Converter
 import converter.ConverterViewModel
 import converter.PDFConverter
+import splitter.SplitterViewModel
 import org.koin.dsl.module
 
 val appModule = module {
@@ -9,4 +10,5 @@ val appModule = module {
     single<Converter> { PDFConverter() }
     single { CombinerViewModel(get()) }
     single { ConverterViewModel(get(), get()) }
+    single { SplitterViewModel(get()) }
 }
